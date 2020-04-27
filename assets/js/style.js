4ddba00e6a526d27de6a5921c8ffaad45c76f3d3
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-27 17:53:58 -0500

    adding base index with materialize

---
+++ assets/js/style.js
@@ -0,0 +1,6 @@
+// materialize initilization for sidebar navigation
+console.log("style loaded")
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
