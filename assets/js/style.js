4b6063652d90e4d69c18f0afc0de349e2650eb19
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-27 23:31:35 -0500

    navbar icons and fontawesome linked

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,3 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+// materialize modals
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.modal');
+var instances = M.Modal.init(elems, {});
+});
