2f2fa1707b5a683d924183b1f7c5ee4a7ae0c47c
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-28 11:07:21 -0500

    google form working on contact page - wireframe for portfolio is working, links on + not set yet

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,12 +1,15 @@
-// materialize initilization for sidebar navigation
+// materialize initilization for sidebar navigation & materialize modals
 console.log("style loaded")
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
   });
 
-// materialize modals
-document.addEventListener('DOMContentLoaded', function() {
-var elems = document.querySelectorAll('.modal');
-var instances = M.Modal.init(elems, {});
-});
+// // materialize modals
+// document.addEventListener('DOMContentLoaded', function() {
+// var elems = document.querySelectorAll('.modal');
+// var instances = M.Modal.init(elems, {});
+// });
+
